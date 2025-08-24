d2fa220df8b52058812a66513d66ed57a90ddd0d
Yasunori Toshimitsu <yasu313nori@gmail.com>
2025-08-24 16:59:14 +0200

    rename mjcf, check dofs before & after conversion

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -3,11 +3,17 @@
 from pathlib import Path
 
 from mjcf_urdf_simple_converter import convert
+import mujoco
 
 
 def test_simple_mjcf_conversion(tmp_path):
     """Convert a minimal MJCF model and verify a URDF file is created."""
-    mjcf_path = Path(__file__).with_name("data") / "simple_mjcf.xml"
-    urdf_path = tmp_path / "simple.urdf"
+    mjcf_path = Path(__file__).with_name("data") / "articulated_mjcf.xml"
+    urdf_path = tmp_path / "articulated.urdf"
     convert(str(mjcf_path), str(urdf_path))
     assert urdf_path.exists()
+
+    # check that exported URDF can at least be loaded in MuJoCo and has same DoF as original MJCF
+    mjcf_model = mujoco.MjModel.from_xml_path(str(mjcf_path))
+    urdf_model = mujoco.MjModel.from_xml_path(str(urdf_path))
+    assert mjcf_model.nv == urdf_model.nv
